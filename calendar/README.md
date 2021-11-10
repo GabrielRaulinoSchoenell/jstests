7cb5cef751075e8c57ccfb3fe0bc01233bad58b2
Gabriel Raulino <gabrielraulinoschoenell@gmail.com>
2021-11-10 00:11:38 -0300

    calendar

---
+++ calendar/README.md
@@ -0,0 +1,30 @@
+# tasks calendar
+this code is a test for my skills in javascript. based in a git repository that gives development ideas
+
+acess this link to see more: 
+https://github.com/florinpop17/app-ideas/blob/master/Projects
+
+## starting
+
+we need to: 
+
+    allow user to create event
+    allow user to edit event
+    allow user to exclude event
+
+    i tought that it'd be really better if the calendar was a traditional calendar. 
+    there're more items such as drag and drop and DB process... i decided to don't do that, focusing more in date()
+
+## what did i learn?
+
+    code efficience
+
+    learned more about dates 
+
+## info
+
+i spent 1 hour 40 minutes doing the code (including html and css)
+
+incredible simple, a lot of work tough
+
+my work is done here!
---
+++ calendar/index.js
@@ -0,0 +1,69 @@
+let weekDay = document.querySelector('.day');
+let calendarTitle = document.querySelector('.calendar-title');
+let calendarDays = document.querySelector('.calendar-days');
+let calendarContent = document.querySelector('.calendar-content');
+let modal = document.querySelector('.modal');
+let modalDay = document.querySelector('.modal-content-day');
+
+
+let date = (year,month,day)=>new Date(year,month,day);
+let dateWeek = (year,month)=>new Date(year,month);
+let initialDay = dateWeek(new Date().getFullYear(),new Date().getMonth()).getDay();
+let daysInMonth = date(new Date().getFullYear(), new Date().getMonth() -1, 0).getDate();
+
+
+
+let week = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
+let months = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december'];
+
+calendarTitle.innerHTML = 'calendar ' + months[new Date().getMonth()] + ' - ' + new Date().getFullYear();
+
+
+for(let i=0; i<6; i++){
+    let clone = weekDay.cloneNode(true);
+    clone.innerHTML = week[i+1];
+    calendarDays.appendChild(clone);
+}
+
+for(let i=0; i<42; i++){
+    let calendarDayBlock = document.createElement('div');
+    calendarDayBlock.classList.add('calendar-content-day');
+    let starter = 0;
+    let unactiveDay ='active';
+    if(initialDay <= i && i < daysInMonth +1) {
+        starter = i;
+        calendarDayBlock.addEventListener('click', createEvent);
+    } else{
+        starter = Math.abs(i - daysInMonth);
+        unactiveDay = 'unactive';
+    };
+    
+
+    calendarDayBlock.innerHTML = '<div class="day-number">'+starter+'</div> <div class="day-content"></div>';
+    calendarDayBlock.classList.add(unactiveDay);
+    
+    calendarDayBlock.childNodes[2].setAttribute('id', i);
+    
+    calendarContent.appendChild(calendarDayBlock);
+
+    
+}
+
+let contentBox;
+function createEvent(e){
+    let day = e.target;
+    contentBox = day.childNodes[2]; 
+
+    modalDay.innerHTML =`${months[new Date().getMonth()]} ${day.childNodes[0].innerHTML}, ${new Date().getFullYear()}`;
+    modal.style.display = 'flex';
+}
+document.querySelector('.exit').addEventListener('click', ()=>{
+    modal.style.display = 'none';
+});
+
+document.querySelector('.save').addEventListener('click',(e)=>{
+    let text = document.querySelector('input').value;
+
+    contentBox.innerHTML = text;
+    modal.style.display = 'none';
+});
